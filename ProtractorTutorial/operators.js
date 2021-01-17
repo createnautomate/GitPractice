276ad9539c4be9a151c4d89a77ff442db02a5e16
createnautomate <emin.hatam84@gmail>
2021-01-16 22:20:56 -0600

    A new file was added Operatioon.js

---
+++ ProtractorTutorial/operators.js
@@ -0,0 +1,41 @@
+/*
++ = sum
+- = sub
+* = mul
+/ = div
+% = remainder
+
+Unary Operators
+++ --> increment operator
+-- --> decrement operator
+! --> logical operator
+
+++operand --> pre increment
+operand++ --> post increment
+
+--operand --> pre decrement
+operand-- --> post decrement
+
+*/
+
+var num1 = 5;
+var num2 = 2;
+console.log(num1 % num2);
+
+var i = 10;
+i = i++;
+console.log(i);
+
+/* console.log(i++); // print 10 incr to 11
+console.log(++i); // incr to 12 print 12
+console.log(++i); // incr to 13 print 13 
+console.log(i); // print 13
+console.log(i++); // print 13 incr to 14 and so on
+console.log(i); // print 14
+console.log('------------');
+console.log(i--); // print 14 decr to 13
+console.log(--i); // decr to 12 print 12
+console.log(--i); // decr to 11 print 11 
+console.log(i); // print 11
+console.log(i--); // decr to 10 print 11
+console.log(i); // print 10 */
